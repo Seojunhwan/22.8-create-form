de575dc1ea99928f39601e55308b10958359cf84
Seojunhwan <seojunhwan@kakao.com>
2021-02-03 14:21:13 +0900

    add logic

--- index.js
+++ index.js
@@ -2,7 +2,94 @@
 const typeSelectBox = document.getElementById("type"),
   thickSelectBox = document.getElementById("thickness"),
   result = document.querySelector(".result"),
-  submitBtn = document.querySelector(".submit_btn");
+  submitBtn = document.querySelector(".submit_btn"),
+  quantity = document.querySelector("#quantity");
+
+function initialOption() {
+  const options = document.querySelectorAll("select > option");
+  options.forEach(function (option) {
+    option.classList.remove("hide");
+  });
+}
+
+function hideOptions() {
+  const mirDisabled = document.querySelector(".mir_disabled");
+  const otherDisabled = document.querySelectorAll(".other_disabled");
+  if (typeSelector() === "Mirror") {
+    initialOption();
+    mirDisabled.classList.add("hide");
+  } else if (
+    typeSelector() === "BLK_HL" ||
+    typeSelector() === "GOL_HL" ||
+    typeSelector() === "GOL_Mirror"
+  ) {
+    initialOption();
+    otherDisabled.forEach(function (option) {
+      option.classList.add("hide");
+    });
+  } else {
+    initialOption();
+  }
+}
+
+function unitPrice() {
+  const type = typeSelector();
+  const thick = thickSelector();
+  if (type === "2B") {
+    if (thick == 0.8) {
+      return 189;
+    }
+    if (thick == 1) {
+      return 227;
+    }
+    if (thick == 1.2) {
+      return 267;
+    }
+    if (thick == 1.5) {
+      return 326;
+    }
+    if (thick == 2) {
+      return 428;
+    }
+  }
+  if (type === "HL") {
+    if (thick == 0.8) {
+      return 210;
+    }
+    if (thick == 1) {
+      return 249;
+    }
+    if (thick == 1.2) {
+      return 293;
+    }
+    if (thick == 1.5) {
+      return 368;
+    }
+    if (thick == 2) {
+      return 480;
+    }
+  }
+  if (type === "Mirror") {
+    if (thick == 1) {
+      return 288;
+    }
+    if (thick == 1.2) {
+      return 335;
+    }
+    if (thick == 1.5) {
+      return 407;
+    }
+    if (thick == 2) {
+      return 530;
+    }
+  }
+  if (type === "BLK_HL") {
+    return 563;
+  }
+  if (type === "GOL_HL" || type === "GOL_Mirror") {
+    return 597;
+  }
+}
 
 function typeSelector() {
   const typeSelect = document.getElementById("type").options.selectedIndex;
@@ -15,19 +102,32 @@ function thickSelector() {
   return thickSelectBox.options[thickSelect].value;
 }
 
-function calcCost(event) {
-  const type = typeSelector();
-  const thick = thickSelector();
-  const width = document.querySelector(".calc_width").value;
-  const height = document.querySelector(".calc_height").value;
-  return type;
+function sizeCalc() {
+  const widthValue = document.querySelector(".calc_width").value;
+  const heightValue = document.querySelector(".calc_height").value;
+  const width = Math.ceil(widthValue / 50) * 50;
+  const height = Math.ceil(heightValue / 50) * 50;
+  const sizeResult = (width * height) / 2500;
+  return sizeResult;
 }
+
+function calcCost() {
+  const result = sizeCalc() * unitPrice() * quantity.value;
+  const calcResult = Math.ceil(result / 100) * 100;
+  return calcResult;
+}
+
+function handleChange() {
+  hideOptions();
+}
+
 function handleSubmit(event) {
   event.preventDefault();
-  result.innerText = calcCost();
+  result.innerText = `고객님께서 선택해주신 옵션은 ${calcCost()} 원 입니다.`;
 }
 
 function init() {
+  typeSelectBox.addEventListener("change", handleChange);
   submitBtn.addEventListener("submit", handleSubmit);
 }
 
