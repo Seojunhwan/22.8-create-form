e78e135aa1f02bcb90c084be23f792172a827740
Seojunhwan <seojunhwan@kakao.com>
2021-03-04 23:36:12 +0900

    support mobile & change result way
    모바일 지원! 그리고 리스트 형식으로 다량의 아이템을 추가하여 확인할 수 있게끔 결과 도출 방식 변경

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const typeSelectBox = document.getElementById("type"),
   optionQuantity = document.querySelector("#quantity"),
   optionWidth = document.querySelector(".option_width"),
   optionLength = document.querySelector(".option_length"),
-  resultBox = document.querySelector(".js-resultBox");
+  resultBox = document.querySelector(".js-resultBox"),
+  naverList = document.querySelector(".naver_item_list");
+
+let validationValueCheck = false;
 
 const outputValueContainer = {};
+let totalPrice = [];
 
-function appearResultBox() {
+const appearResultBox = () => {
   resultBox.classList.replace("hide", "active");
-}
+};
 
-function hideOptions() {
+const hideOptions = () => {
   const thicknessList = document.querySelector("#thickness");
   thicknessList[1].classList.remove("hide-option");
   thicknessList[2].classList.remove("hide-option");
@@ -33,9 +37,9 @@ function hideOptions() {
     thicknessList[4].classList.add("hide-option");
     thicknessList[5].classList.add("hide-option");
   }
-}
+};
 
-function inputValueSet() {
+const inputValueSet = () => {
   const inputValueContainer = {
     type: optionType.value,
     thickness: parseFloat(optionThickness.value),
@@ -44,10 +48,10 @@ function inputValueSet() {
     length: parseFloat(optionLength.value),
   };
   return inputValueContainer;
-}
+};
 
-function calcValue(value) {
-  function unitPrice() {
+const calcValue = (value) => {
+  const unitPrice = () => {
     if (value.type === "2B") {
       if (value.thickness == 0.8) {
         return 189;
@@ -102,28 +106,28 @@ function calcValue(value) {
     if (value.type === "GOL_HL" || value.type === "GOL_Mirror") {
       return 597;
     }
-  }
-  function calcSize() {
+  };
+  const calcSize = () => {
     const calcWidth = Math.ceil(value.width / 50) * 50;
     const calcLength = Math.ceil(value.length / 50) * 50;
     const calcResult = (calcWidth * calcLength) / 2500;
     return calcResult;
-  }
-  function calcPlateCost() {
+  };
+  const calcPlateCost = () => {
     const result = Math.ceil((unitPrice() * calcSize()) / 100) * 100;
     if (result < 1000) {
       return 1000;
     } else {
       return result;
     }
-  }
-  function calcFoundationCost() {
+  };
+  const calcFoundationCost = () => {
     if (calcPlateCost() < 10000) {
       return 1000;
     } else {
       return 0;
     }
-  }
+  };
   const calcPrice = (calcPlateCost() + calcFoundationCost()) * value.quantity;
   const calcNaverQuantity = calcPrice / 100;
   outputValueContainer.price = calcPrice;
@@ -131,9 +135,9 @@ function calcValue(value) {
   outputValueContainer.foundationCost = calcFoundationCost();
   outputValueContainer.quantity = value.quantity;
   outputValueContainer.naverQuantity = calcNaverQuantity;
-}
+};
 
-function appendValue(output) {
+const appendValue = (output) => {
   const resultList = document.querySelectorAll(".result");
   const naverProductInfo = document.querySelector(".js-naver-product__info");
   const naverQuantityResult = document.querySelector(".js-quantity");
@@ -141,23 +145,123 @@ function appendValue(output) {
   resultList[0].innerText = `고객님께서 선택해주신 옵션은 ${output.price} 원 입니다.`;
   resultList[1].innerText = `판재비 ${output.plateCost} 원 + 재단비 ${output.foundationCost} 원 X ${output.quantity} 개`;
   resultList[2].innerHTML = `하기와 같이 네이버 스토어 구매 수량에 ${output.naverQuantity}개 입력해 주시기 바랍니다.`;
-  naverProductInfo.innerText = `${inputValueSet().width} / ${
+
+  // FIXME:리스트 형식으로 바꿔서 필요 없는 코드
+  // naverProductInfo.innerText = `${inputValueSet().width} / ${
+  //   inputValueSet().length
+  // } / ${inputValueSet().type} / ${inputValueSet().thickness} T`;
+  // naverQuantityResult.innerText = output.naverQuantity;
+  // naverPriceResult.innerText = `${output.price} 원`;
+};
+
+const appendOptions = (output) => {
+  const naverItem = document.createElement("li");
+  const naverBox = document.createElement("div");
+  const naverBoxOption = document.createElement("div");
+  const product__info = document.createElement("div");
+  const tempDiv = document.createElement("div");
+
+  naverItem.className = "naver_item";
+  naverBox.className = "naver_box";
+  naverBoxOption.className = "naver_box--option";
+  product__info.className = "js-naver-product__info";
+
+  product__info.innerText = `${inputValueSet().width} / ${
     inputValueSet().length
   } / ${inputValueSet().type} / ${inputValueSet().thickness} T`;
-  naverQuantityResult.innerText = output.naverQuantity;
-  naverPriceResult.innerText = `${output.price} 원`;
-}
 
-function handleSubmit(event) {
-  appearResultBox();
-  calcValue(inputValueSet());
-  hideOptions();
-  appendValue(outputValueContainer);
-}
+  const naverBoxAdjustment = document.createElement("div");
+  const adjustQuantity = document.createElement("div");
+  const operMinus = document.createElement("div");
+  const operPlus = document.createElement("div");
+  const quantity = document.createElement("div");
+  const naverBoxPrice = document.createElement("div");
+  const naverBoxPriceSpan = document.createElement("span");
 
-function init() {
+  naverBoxAdjustment.className = "naver_box--adjustment";
+  adjustQuantity.className = "adjust--quantity";
+  operMinus.className = "operator";
+  operPlus.className = "operator";
+  quantity.className = "quantity";
+  naverBoxPrice.className = "naver-box--price";
+  naverBoxPriceSpan.className = "naver-box--price-value";
+
+  quantity.innerText = output.naverQuantity;
+  naverBoxPriceSpan.innerText = `${output.price} 원`;
+
+  adjustQuantity.appendChild(operMinus);
+  adjustQuantity.appendChild(quantity);
+  adjustQuantity.appendChild(operPlus);
+
+  naverBoxOption.appendChild(product__info);
+  naverBoxOption.appendChild(tempDiv);
+  naverBox.appendChild(naverBoxOption);
+  naverItem.appendChild(naverBox);
+  naverList.appendChild(naverItem);
+
+  naverBoxPrice.appendChild(naverBoxPriceSpan);
+  naverBoxAdjustment.appendChild(adjustQuantity);
+  naverBoxAdjustment.appendChild(naverBoxPrice);
+  naverBox.appendChild(naverBoxAdjustment);
+};
+
+const validationValue = () => {
+  if (
+    optionType.value === "a" ||
+    optionThickness.value === "a" ||
+    optionQuantity.value === "" ||
+    optionWidth.value === "" ||
+    optionLength.value === ""
+  ) {
+    alert("옵션을 선택해 주세요!");
+  } else {
+    validationValueCheck = true;
+  }
+};
+
+const resetValue = () => {
+  optionType.value = "a";
+  optionThickness.value = "a";
+  optionQuantity.value = null;
+  optionWidth.value = null;
+  optionLength.value = null;
+  validationValueCheck = false;
+  // FIXME:여기 확인 필요
+};
+
+const calcTotalPrice = () => {
+  const itemPrice = document.querySelectorAll(".naver-box--price-value");
+  const naverTotalPrice = document.querySelector(".total-price--value");
+  if (itemPrice.length >= 2) {
+    itemPrice.forEach((item) => {
+      let res = item.innerText.replace(/[^0-9]/g, "");
+      totalPrice.push(parseInt(res));
+    });
+  } else {
+    let res = itemPrice[0].innerHTML.replace(/[^0-9]/g, "");
+    totalPrice.push(parseInt(res));
+  }
+  const reducer = (acc, current) => acc + current;
+  naverTotalPrice.innerText = `Total : ${totalPrice.reduce(reducer)}원`;
+  totalPrice = [];
+};
+
+const handleSubmit = (event) => {
+  validationValue();
+  if (validationValueCheck === true) {
+    appearResultBox();
+    hideOptions();
+    calcValue(inputValueSet());
+    appendValue(outputValueContainer);
+    appendOptions(outputValueContainer);
+    validationValueCheck = false;
+    calcTotalPrice();
+  }
+};
+
+const init = () => {
   typeSelectBox.addEventListener("change", hideOptions);
   submitBtn.addEventListener("click", handleSubmit);
-}
+};
 
 init();
